0e8e881d5f1a6cc4b6fb2b4fce1bf7c8b2d7aa99
Paul Bovis <paulbovisblahblah@gmail.com>
2019-01-27 13:17:38 -0700

    Add ability to switch the default address

--- components/checkout/addresses/ShippingAddress.vue
+++ components/checkout/addresses/ShippingAddress.vue
@@ -3,19 +3,44 @@
       <div class="message-body">
         <h1 class="title is-5">Ship to</h1>
 
-        <template v-if="selectedAddress">
-        	{{ selectedAddress.name }}<br>
-        	{{ selectedAddress.address_1 }}<br>
-        	{{ selectedAddress.city }}<br>
-        	{{ selectedAddress.postal_code }}<br>
-        	{{ selectedAddress.country.name }}
+        <template v-if="selecting">
+        	<shipping-address-selector />
         </template>
+
+		<template v-else>
+	        <template v-if="selectedAddress">
+	        	{{ selectedAddress.name }}<br>
+	        	{{ selectedAddress.address_1 }}<br>
+	        	{{ selectedAddress.city }}<br>
+	        	{{ selectedAddress.postal_code }}<br>
+	        	{{ selectedAddress.country.name }}<br>
+
+	        	<br>
+	        </template>
+
+	        <div class="field is-grouped">
+	        	<p class="control">
+	        		<button 
+	        			class="button is-info"
+	        			@click="selecting = true"
+	        		>
+	        			Change shipping address
+	        		</button>
+	        	</p>
+	        </div>
+        </template>        
       </div>
     </article>
 </template>
 
 <script>
+	import ShippingAddressSelector from './ShippingAddressSelector'
+
 	export default {
+		components: {
+			ShippingAddressSelector
+		},
+
 		props: {
 			addresses: {
 				type: Array,
@@ -26,7 +51,9 @@
 		data () {
 			return {
 				localAddresses: this.addresses,
-				selectedAddress: null
+				selectedAddress: null,
+				selecting: false,
+				created: false
 			}
 		},
 
